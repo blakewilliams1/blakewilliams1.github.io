120d14bd5aa70a2157bb977ee9180ac54ba4d95a
blakewilliams1 <blake@blakewilliams.org>
2021-05-14 17:27:38 -0700

    Anther routing fix attempt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     component: QRLandingPage,
   },
   {
-    path: 'qr/',
+    path: 'qr/.',
     component: QRLandingPage,
   },
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomePageModule } from './homepage/home_page_module';
 import { QRLandingPageModule } from './qr/qr_landing_page_module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Location } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -23,3 +24,19 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+const __stripTrailingSlash = (Location as any).stripTrailingSlash;
+Location.stripTrailingSlash = function (url) {
+  if (url.endsWith('/')) {
+    url = url;
+  }
+  else {
+    url = url + '/';
+  }
+  const queryString$ = url.match(/([^?]*)?(.*)/) || ['', '', ''];
+  if (queryString$[2].length > 0) {
+    return /[^\/]\/$/.test(queryString$[1]) ? queryString$[1] + '.' + queryString$[2] : __stripTrailingSlash(url);
+  }
+
+  return /[^\/]\/$/.test(url) ? url + '.' : __stripTrailingSlash(url);
+};
