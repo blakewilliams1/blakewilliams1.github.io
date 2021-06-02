b404d5fb040486fb3d20c2ae77aa8dc2634fb323
blakewilliams1 <blake@blakewilliams.org>
2021-06-01 22:37:17 -0700

    fixing AR barcode numbering

--- src/app/qr/qr_landing_page_module.ts
+++ src/app/qr/qr_landing_page_module.ts
@@ -1,4 +1,4 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from '@angular/platform-browser';
 import { QRLandingPage } from "./qr_landing_page";
@@ -12,6 +12,5 @@ import { QRLandingPage } from "./qr_landing_page";
         HttpClientModule
     ],
     exports: [QRLandingPage],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class QRLandingPageModule { }
