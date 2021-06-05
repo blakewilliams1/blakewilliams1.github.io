8f1cb593d9bde04818e3b938a8be6a2634ee18d7
blakewilliams1 <blake@blakewilliams.org>
2021-06-05 16:13:45 -0700

    Adding first pass at vertical timeline element

---
+++ src/app/sharedcomponents/verticaltimeline/vertical_timeline.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { SafeHtml } from '@angular/platform-browser';
+
+@Component({
+    selector: 'timeline',
+    templateUrl: './vertical_timeline.html',
+    styleUrls: ['./vertical_timeline.scss']
+})
+export class TimelineComponent {
+
+    @Input()
+    timelineEvents: TimelineEvent[] = [];
+
+    calculateTimelineDate(date: Date): string {
+        return new Intl.DateTimeFormat('en-US', {
+            year: 'numeric',
+            month: 'long',
+        }).format(date);
+    }
+}
+
+export interface TimelineEvent {
+    date: Date;
+    content: SafeHtml;
+}
