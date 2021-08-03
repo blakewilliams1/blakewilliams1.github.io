517ed10c892066cb5159f93264c8876d693565fe
blakewilliams1 <blake@blakewilliams.org>
2021-08-03 02:08:02 -0700

    Adding language skills gauges

--- src/app/homepage/home_page.ts
+++ src/app/homepage/home_page.ts
@@ -9,6 +9,38 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
     @ViewChild('tabgroup') tabGroup!: MatTabGroup;
+    readonly languageSkills: Skill[] = [
+        {label: 'Java', percent: 90},
+        {label: 'Typescript', percent: 80},
+        {label: 'Javascript', percent: 80},
+        {label: 'C++', percent: 70},
+        {label: 'Dart', percent: 40},
+        {label: 'Kotlin', percent: 30},
+    ];
 
-    constructor(private readonly router: Router) {}
+    // Gets a descriptive word to match the skill percentage.
+    getSkillDescriptor(percent: number): string {
+        if (percent >= 95) {
+            return 'Master';
+        } else if (percent >=80) {
+            return 'Advanced';
+        } else if (percent >=60) {
+            return 'Proficient';
+        } else if (percent >=40) {
+            return 'Competant';
+        } else if (percent >=20) {
+            return 'Novice';
+        } else {
+            return 'Beginner';
+        }
+    }
+
+    constructor(private readonly router: Router) {
+        console.log('(͡ ͡° ͜ つ ͡͡°) nosey now, aren\'t we?')
+    }
+}
+
+interface Skill {
+    label: string;
+    percent: number;
 }
