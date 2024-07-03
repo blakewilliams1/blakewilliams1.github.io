58d5febbe2c8b25d9c3366f2b31335271bebb093
blakewilliams1 <blake@blakewilliams.org>
2024-07-03 00:24:08 -0700

    Adding thumbnails to travels and projects entries

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,10 +35,6 @@ const routes: Routes = [
     loadChildren: () => import('./travels/mauritania_trip/mauritania_trip_page_module').then(m => m.MauritaniaTripPageModule)
   },
   {
-    path: 'road_to_ushuaia',
-    redirectTo: 'road-to-ushuaia',
-  },
-  {
     path: 'road-to-ushuaia',
     loadChildren: () => import('./travels/road_to_ushuaia/road_to_ushuaia_page_module').then(m => m.RoadToUshuaiaPageModule)
   },
