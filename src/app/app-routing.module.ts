b382ec70f04d70f5cf8b7d8e4b83b30b790b8742
blakewilliams1 <blake@blakewilliams.org>
2024-07-04 12:51:24 -0700

    Fixing issue with scroll position info bleeding between pages
    The new property name isn't intuitive, but the idea is that the scroll should now be reset to top of page on route change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -58,7 +58,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: false})],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: false,
+    scrollPositionRestoration: 'enabled',
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
