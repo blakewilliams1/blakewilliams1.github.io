fe1456b1cee8aa4a9daa4b7d60649514bd091b74
blakewilliams1 <blake@blakewilliams.org>
2024-11-26 16:12:58 -0800

    Improving Ushuaia trip redirect experience
    kinda hacky but whatever

--- src/app/travels/road_to_ushuaia/road_to_ushuaia_page.ts
+++ src/app/travels/road_to_ushuaia/road_to_ushuaia_page.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'road-to-ushuaia-page',
   templateUrl: './road_to_ushuaia_page.html',
  styleUrls: ['./road_to_ushuaia_page.scss']
 })
-export class RoadToUshuaiaPage { 
-  constructor() {
+export class RoadToUshuaiaPage {
+  constructor(private readonly router: Router) {
     setTimeout(() => {
-      window.location.href = 'https://advrider.com/f/threads/california-to-ushuaia-2024.1750647/page-1000';
+      setTimeout(() => {
+        router.navigate(['/travels']);
+      }, 100);
+      window.open('https://advrider.com/f/threads/california-to-ushuaia-2024.1750647/page-1000', '_blank');
     }, 2000);
   }
 }
