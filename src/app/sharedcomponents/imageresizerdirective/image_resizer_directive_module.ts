39c5e7a2f70a6ebe6df72a8842859d8b391c3500
blakewilliams1 <blake@blakewilliams.org>
2024-11-27 16:01:53 -0800

    Removing unnecessary import

--- src/app/sharedcomponents/imageresizerdirective/image_resizer_directive_module.ts
+++ src/app/sharedcomponents/imageresizerdirective/image_resizer_directive_module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ImageResizerDirective } from 'src/app/sharedcomponents/imageresizerdirective/image_resizer_directive';
 
 @NgModule({
   declarations: [
     ImageResizerDirective,
-  ],
-  imports: [
-      CommonModule,
   ],
   exports: [ImageResizerDirective],
 })
