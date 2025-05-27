90608503597fc2dd914129a3c5d83fc41097ccad
blakewilliams1 <blake@blakewilliams.org>
2025-05-27 16:32:34 -0700

    Replacing language skills on homepage

--- src/app/homepage/home_page.ts
+++ src/app/homepage/home_page.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { KonamiCodeService } from '../sharedcomponents/konamicodeservice/konami_code_service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { Component, ElementRef, Renderer2, ViewChild, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
     selector: 'home-page',
@@ -12,47 +12,73 @@ import { MatCardModule } from '@angular/material/card';
     imports: [
         CommonModule,
         MatCardModule,
+        HttpClientModule,
     ],
 })
-export class HomePage {
-    readonly languageSkills: Skill[] = [
-        {label: 'Java', percent: 90},
-        {label: 'Typescript', percent: 80},
-        {label: 'Javascript', percent: 80},
-        {label: 'C++', percent: 70},
-        {label: 'Kotlin', percent: 40},
-        {label: 'Dart', percent: 30},
-    ];
+export class HomePage implements AfterViewInit {
+  @ViewChild('actionContainer') actionContainer: ElementRef;
+  @ViewChild('actions') actionsList: ElementRef;
+  readonly CONFIG_FILE_URL =
+    'https://raw.githubusercontent.com/blakewilliams1/blakewilliams1.github.io/main/src/assets/current_activities.txt';
+	currentActivities: string[] = [
+		'first',
+		'second',
+		'third',
+		'fourth',
+		'fifth'
+	];
+  isBrowser: boolean;
+	private readonly ACTION_CHANGE_INTERVAL_MS = 5000;
+	readonly scrollingTask = () => {
+		const actions: Element[] = this.actionsList.nativeElement.children;
+		// Calculate the vertical distance between the top of the container for the actions and the action about to be
+		// displayed.
+		const containerOffsetTopPx = (actions[0] as HTMLElement).offsetTop;
+		const targetActionIndex = Math.floor(this.currentActivities.length * Math.random());
+		const targetChild = this.actionsList.nativeElement.children[targetActionIndex];
+		const yTranslateOffset = -((targetChild as HTMLElement).offsetTop - containerOffsetTopPx);
+		// Ensure that the viewport into the current action is tall enough to show the entry if it wraps.
+		const height = (targetChild as HTMLElement).offsetHeight;
+		this.renderer.setStyle(
+				this.actionsList.nativeElement,
+				'height',
+				`${height}px`);
 
-    // Gets a descriptive word to match the skill percentage.
-    getSkillDescriptor(percent: number): string {
-        if (percent >= 95) {
-            return 'Master';
-        } else if (percent >=80) {
-            return 'Advanced';
-        } else if (percent >=60) {
-            return 'Proficient';
-        } else if (percent >=40) {
-            return 'Competent';
-        } else if (percent >=20) {
-            return 'Novice';
-        } else {
-            return 'Beginner';
-        }
-    }
+		// Shift all the actions up or down based on the calculated amount to show the the current one.
+		for (const child of actions) {
+			this.renderer.setStyle(
+					child,
+					'transform',
+					`translateY(${yTranslateOffset}px)`);
+		}
 
-    constructor(
-        private readonly router: Router,
-        private readonly konamiService: KonamiCodeService) {
-    }
+		// Re-trigger the same task again at the desired interval.
+		setTimeout(this.scrollingTask, this.ACTION_CHANGE_INTERVAL_MS);
+	}
 
-    ngAfterViewInit () {
-        console.log('(͡ ͡° ͜ つ ͡͡°) nosey now, aren\'t we?')
-        this.konamiService.registerListener();
-    }
-}
+	constructor(
+			private renderer: Renderer2,
+			private readonly konamiService: KonamiCodeService,
+			@Inject(PLATFORM_ID) private platformId: Object,
+			private readonly httpClient: HttpClient) {
+		this.isBrowser = isPlatformBrowser(this.platformId);
+	}
 
-interface Skill {
-    label: string;
-    percent: number;
+	ngAfterViewInit () {
+		console.log('(͡ ͡° ͜ つ ͡͡°) nosey now, aren\'t we?')
+		// Prevent hydration breaking errors by only modifying the DOM if this is being ran client side in the browser.
+		if (this.isBrowser) {
+		this.konamiService.registerListener();
+
+		// Load the contents of the txt file containing all the current activities, then save it as a class member.
+		this.httpClient.get(this.CONFIG_FILE_URL, {responseType: 'text'})
+					.subscribe((response) => {
+						this.currentActivities = response.split('\n').filter(str => str.trim() !== "") || [];
+					});
+
+					// Once we've acquired the list of actions I'm 'currently doing', initiate the repeating call to scroll
+					// through them all.
+					this.scrollingTask();
+		}
+	}
 }
