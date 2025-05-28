238585d226053efb130658f5820c563ad42cdaa1
blakewilliams1 <blake@blakewilliams.org>
2025-05-28 10:10:48 -0700

    Improving presentation order of activities on home page
    Now it shows all activities once before repeating any of them

--- src/app/homepage/home_page.ts
+++ src/app/homepage/home_page.ts
@@ -18,22 +18,59 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 export class HomePage implements AfterViewInit {
   @ViewChild('actionContainer') actionContainer: ElementRef;
   @ViewChild('actions') actionsList: ElementRef;
+	private readonly ACTION_CHANGE_INTERVAL_MS = 4000;
   readonly ACTIVITIES_LIST_URL =
     'https://raw.githubusercontent.com/blakewilliams1/blakewilliams1.github.io/main/src/assets/current_activities.txt';
-	currentActivities: string[] = [''];
+	private activityRandomIndexOrder: number[] = [];
 	// Guards against client-side-only code from being ran server-side.
-  isBrowser: boolean;
-	private readonly ACTION_CHANGE_INTERVAL_MS = 4000;
-	readonly scrollingTask = () => {
+  private isBrowser: boolean;
+	allCurrentActivities: string[] = [''];
+
+	constructor(
+			private renderer: Renderer2,
+			private readonly konamiService: KonamiCodeService,
+			@Inject(PLATFORM_ID) private platformId: Object,
+			private readonly httpClient: HttpClient) {
+		this.isBrowser = isPlatformBrowser(this.platformId);
+	}
+
+	ngAfterViewInit () {
+		// Prevent hydration breaking errors by only modifying the DOM if this is being ran client side in the browser.
+		if (this.isBrowser) {
+			this.konamiService.registerListener();
+
+			// Load the contents of the txt file containing all the current activities, then save it as a class member.
+			this.httpClient.get(this.ACTIVITIES_LIST_URL, {responseType: 'text'})
+					.subscribe((response) => {
+						this.allCurrentActivities = response.split('\n').filter(str => str.trim() !== "") || [];
+
+						// Once we've acquired the list of actions I'm 'currently doing', initiate the repeating call to scroll
+						// through them all. setTimeout() is needed to schedule the animations in next update loop, after
+						// this.allCurrentActivities has been set.
+						setTimeout(() => this.scrollingTask());
+					});
+		}
+	}
+
+	private scrollingTask() {
 		const actions: Element[] = this.actionsList.nativeElement.children;
 		const containerOffsetTopPx = (actions[0] as HTMLElement).offsetTop;
-		// Select a new index of an action to show on screen that is unique from the current one.
-		let targetActionIndex = -1;
-		do {
-			targetActionIndex = Math.floor(this.currentActivities.length * Math.random());
-		}		while (this.previousTargetActionIndex == targetActionIndex && actions.length > 1);
-		this.previousTargetActionIndex = targetActionIndex;
- 
+		// Refill the activity presentation order list if it's empty again. This ensures no repeats till all have been
+		// shown once.
+		if (this.activityRandomIndexOrder.length == 0) {
+			// Randomize the order of the array to uniquely show all, but in a random order before repeats.
+			this.activityRandomIndexOrder = Array.from({ length: this.allCurrentActivities.length }, (_, i) => i);
+			for (let i = this.activityRandomIndexOrder.length - 1; i > 0; i--) {
+				const swapIndex = Math.floor(Math.random() * (this.activityRandomIndexOrder.length));
+				const valueA = this.activityRandomIndexOrder[i];
+				const valueB = this.activityRandomIndexOrder[swapIndex];
+				this.activityRandomIndexOrder[i] = valueB;
+				this.activityRandomIndexOrder[swapIndex] = valueA;
+			}
+		}
+	
+		// The default value is never expected tyo be used, but is here to satisfy the static compiler.
+		const targetActionIndex: number = this.activityRandomIndexOrder.pop() || 0;
 		// Calculate the vertical distance between the top of the container for the actions and the action about to be
 		// displayed.
 		const targetChild = this.actionsList.nativeElement.children[targetActionIndex];
@@ -54,34 +91,6 @@ export class HomePage implements AfterViewInit {
 		}
 
 		// Re-trigger the same task again at the desired interval.
-		setTimeout(this.scrollingTask, this.ACTION_CHANGE_INTERVAL_MS);
-	}
-	private previousTargetActionIndex = 0;
-
-	constructor(
-			private renderer: Renderer2,
-			private readonly konamiService: KonamiCodeService,
-			@Inject(PLATFORM_ID) private platformId: Object,
-			private readonly httpClient: HttpClient) {
-		this.isBrowser = isPlatformBrowser(this.platformId);
-	}
-
-	ngAfterViewInit () {
-		console.log('(͡ ͡° ͜ つ ͡͡°) nosey now, aren\'t we?')
-		// Prevent hydration breaking errors by only modifying the DOM if this is being ran client side in the browser.
-		if (this.isBrowser) {
-			this.konamiService.registerListener();
-
-			// Load the contents of the txt file containing all the current activities, then save it as a class member.
-			this.httpClient.get(this.ACTIVITIES_LIST_URL, {responseType: 'text'})
-					.subscribe((response) => {
-						this.currentActivities = response.split('\n').filter(str => str.trim() !== "") || [];
-
-						// Once we've acquired the list of actions I'm 'currently doing', initiate the repeating call to scroll
-					// through them all. setTimeout() is needed to schedule the animations in next update loop, after
-					// this.currentActivities has been set.
-					setTimeout(() => this.scrollingTask());
-					});
-		}
+		setTimeout(() => this.scrollingTask(), this.ACTION_CHANGE_INTERVAL_MS);
 	}
 }
