d1f55003e9a9cb1879060c9cc8c5f8a4789ca86d
blakewilliams1 <blake@blakewilliams.org>
2025-05-29 16:25:08 -0700

    Auto-generated commit

---
+++ chunk-L476G3VM.js
@@ -0,0 +1,60 @@
+import{a as c,c as p,e as u,f as g}from"./chunk-US4Z7JDK.js";import"./chunk-ORYLB4O6.js";import{Fa as a,La as o,ab as s,fb as i,ga as m,gb as e,hb as n,ub as t,xb as h}from"./chunk-NEKH66XV.js";var S=(()=>{class l{constructor(){this.pornhubCss=`
+  /* step fantasy stuff */
+  a[title~="step" i],
+  a[title~="stepmom" i],
+  a[title~="stepdad" i],
+  a[title~="stepsis" i],
+  a[title~="stepsister" i],
+  a[title~="stepbro" i],
+  a[title~="stepbrother" i],
+  a[title~="stepdaughter" i],
+  a[title~="stepcousin" i],
+  a[data-title~="step" i],
+  a[data-title~="stepmom" i],
+  a[data-title~="stepdad" i],
+  a[data-title~="stepsis" i],
+  a[data-title~="stepsister" i],
+  a[data-title~="stepbro" i],
+  a[data-title~="stepbrother" i],
+  a[data-title~="stepdaughter" i],
+  a[data-title~="stepcousin" i] {
+    visibility: hidden;
+  }
+  `,this.spanishDictCss=`
+  /* Limited page view popup. */
+  .ReactModalPortal {
+    visibility: hidden;
+  }
+  `,this.twitterCss=`
+  article:has([data-testid="socialContext"]) {
+    display: none;
+  }
+  `,this.chatgptCss=`
+  body {
+    pointer-events: auto;
+  }
+
+  div[data-testid=modal-no-auth-rate-limit] {
+    visibility: hidden;
+    display: none;
+  }
+  `,this.youtubeCss=`
+  ytd-emergency-onebox-renderer {
+    display: none;
+  }
+  
+  /* Rows of youtube shorts on the search page */
+  ytd-reel-shelf-renderer {
+    display: none !important;
+  }
+  
+  /* Rows of youtube shorts on the home page */
+  div#dismissible:has(span#title) {
+    display: none;
+  }
+  
+  /* Misc bullshit messaging */
+  .ytd-statement-banner-renderer {
+      display: none;
+  }
+  `}static{this.\u0275fac=function(d){return new(d||l)}}static{this.\u0275cmp=m({type:l,selectors:[["css-injections-page"]],standalone:!0,features:[h],decls:70,vars:5,consts:[[1,"divider"],[1,"disclaimer"],["href","https://chrome.google.com/webstore/detail/user-javascript-and-css/nbhcbdghjpllgmfilhnhkllmkecfmpld","target","_blank"],["href","https://blog.logrocket.com/product-management/why-youtube-created-shorts/","target","_blank"],[1,"code-container"],[3,"innerHTML"],["href","https://developer.mozilla.org/en-US/docs/Web/CSS/:has","target","_blank"]],template:function(d,r){d&1&&(i(0,"mat-card")(1,"mat-card-header")(2,"h1"),t(3,"CSS Injection"),e()(),n(4,"div",0),i(5,"mat-card-content")(6,"p",1),t(7,"DISCLAIMER: I do not own the chrome extension, know it's developer, or have been paid to endorse it. My thoughts are my own."),e(),i(8,"p"),t(9,` While ad blockers can make browsing the web a significantly different experience than without, there is still a lot of content that I don't like seeing online. Not that there is anything inherently wrong with the things I don't enjoy viewing, it's just my personal preferences. I thought "gee, wouldn't it be great if there was a Chrome extension that could inject custom CSS rules you create into websites of your choosing to hide things you don't want to see?" Sure enough, there are several extensions made to do just that! I tried a few and found one that works well and has a nice UI. `),e(),i(10,"p")(11,"a",2),t(12," User JavaScript and CSS "),e(),t(13," is a great extension that I've been using to great effect. Here are a few of my favorite custom CSS rules I've created for myself to make browsing certain websites more enjoyable. Here are a few of my favorite custom CSS rules I've created for myself to make browsing certain websites more enjoyable. "),e(),i(14,"h2"),t(15,"Youtube.com"),e(),i(16,"p"),t(17,"I really don't like Youtube Shorts, and the site force-feeds you them constantly. There is no way to disable Shorts as "),i(18,"a",3),t(19,"the company seems pressured to compete with Tiktok."),e(),t(20," I don't think it encourages healthy, intentional, or mindful viewing and it feels more like brainrot when you viewing Shorts, as if the Tiktok content model is something to aspire to copy. These injections will hide them from your view."),e(),i(21,"pre",4),n(22,"code",5),e(),i(23,"h2"),t(24,"Twitter.com"),e(),i(25,"p"),t(26,"I don't want to see what the people I follow like and retweet. I want to see "),i(27,"i"),t(28,"their"),e(),t(29," content!! Most of the retweets and likes from other people I see tend to be political, hot takes, or tiktok reposts. I don't miss this spam for the most part."),e(),i(30,"pre",4),n(31,"code",5),e(),i(32,"h2"),t(33,"Chatgpt.com"),e(),i(34,"p"),t(35," I simply don't want to log in with my Google account, nor create an account. This clears the modals and allows usage of the basic models. "),e(),i(36,"pre",4),n(37,"code",5),e(),i(38,"h2"),t(39,"Pornhub.com"),e(),i(40,"p"),t(41,"I'm not a fan of 'step fantasy' porn, where the actors pretend to be related. I really don't get this taboo appeal, but it's all over the platform. Sure you can set a Taste Profile if you have even a free account, but this only blocks categorical content on the My Recommendations page. Taste Profile doesn't apply when using the site in any other way than that."),e(),i(42,"pre",4),n(43,"code",5),e(),i(44,"h2"),t(45,"Spanishdict.com"),e(),i(46,"p"),t(47,"This is a great website for viewing verb conjugations if you're learning spanish. This removes a modal that blocks the screen on page load"),e(),i(48,"pre",4),n(49,"code",5),e(),i(50,"h2"),t(51,"Shortcomings"),e(),i(52,"p"),t(53,"This (and any CSS injector) is not without it's shortcomings. There are some things that it can struggle with such as:"),e(),i(54,"ul")(55,"li")(56,"b"),t(57,"High barrier to entry:"),e(),t(58," The percent of people who know how to write CSS selectors is relatively small, all things considered. "),e(),i(59,"li")(60,"b"),t(61,"Easily broken when a website remodels:"),e(),t(62," If a website overhauls itself, there is a good chance your selectors will break. These little 'hacks' usually are very dependent on the current iteration of the website's UI. "),e(),i(63,"li")(64,"b"),t(65,"Difficult to create well:"),e(),t(66," Inspecting the DOM of most big name websites reveal a lot of generated elements, random class names, and general obfuscation/minification. You rarely ever get so lucky as to find a <div> with a human readable and obvious class name. I think the "),i(67,"a",6),t(68,":has selector"),e(),t(69," will significantly help with this once it lands (assuming it's reasonably performant). "),e()()()()),d&2&&(o(22),s("innerHTML",r.youtubeCss,a),o(9),s("innerHTML",r.twitterCss,a),o(6),s("innerHTML",r.chatgptCss,a),o(6),s("innerHTML",r.pornhubCss,a),o(6),s("innerHTML",r.spanishDictCss,a))},dependencies:[g,c,p,u],styles:["mat-card[_ngcontent-%COMP%]{margin:20px 5%;max-width:1400px}mat-card-title[_ngcontent-%COMP%]{margin-top:20px!important}.disclaimer[_ngcontent-%COMP%]{display:block;margin-bottom:20px;font-size:1.1rem}.divider[_ngcontent-%COMP%]{border-top:1px solid white;margin-bottom:1em}.code-container[_ngcontent-%COMP%]{max-width:fit-content;overflow-x:auto;width:auto}pre[_ngcontent-%COMP%]{background:#303030;border-radius:8px;padding-right:16px;width:fit-content}"]})}}return l})();export{S as CssInjectionPage};
