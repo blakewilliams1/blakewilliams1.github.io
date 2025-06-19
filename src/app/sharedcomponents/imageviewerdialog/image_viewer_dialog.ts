daeaa4140e71c792cb6d0c63506149656f7461d0
blakewilliams1 <blake@blakewilliams.org>
2025-06-18 21:07:30 -0700

    Adding zoom/drag ability for image viewing dialog

--- src/app/sharedcomponents/imageviewerdialog/image_viewer_dialog.ts
+++ src/app/sharedcomponents/imageviewerdialog/image_viewer_dialog.ts
@@ -1,22 +1,121 @@
-import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, inject, OnDestroy, Renderer2, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   standalone: true,
   selector: 'image-viewer-dialog',
-  template: '<img [src]="data.imageUrl" (click)="closeDialog()"/>',
+  template: `<div><img #image id="image" [src]="data.imageUrl" (click)="closeDialog()"/></div>`,
   styles: `
-    img {
+    div {
+      overflow: hidden;
       max-width: 95vw;
       max-height: 95vh;
     }
+    img {
+      width: 100%;
+    }
   `,
+    imports: [
+        CommonModule,
+    ],
 })
-export class ImageViewerDialog {
+export class ImageViewerDialog implements AfterViewInit, OnDestroy {
+  @ViewChild('image') image!: ElementRef;
   data = inject(MAT_DIALOG_DATA);
   dialogRef = inject(MatDialogRef<ImageViewerDialog>);
+  private startX: number = 0;
+  private startY: number = 0;
+  private scale: number = 1;
+  private isDragging: boolean = false;
+  private offsetX: number = 0;
+  private offsetY: number = 0;
+  
+  constructor(private readonly renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    document.addEventListener('dragstart', this.onDragStart);
+    document.addEventListener('drag', this.onDrag);
+    document.addEventListener('dragend', this.onDragEnd);
+    document.addEventListener('wheel', this.onWheelScroll);
+    this.updateCursorStyle();
+  }
+
+  private onWheelScroll = (event: WheelEvent) => {
+    // Zoom in multiplicative by 1.1x so deeper zooming doesn't feel weak.
+    const increment = event.deltaY > 0 ? 1/1.1 : 1.1;
+    this.scale = Math.max(1, Math.min(this.scale * increment, 10));
+    this.updateImagePosition();
+  }
+
+  private onDragStart = (event: DragEvent) => {
+    this.startX = event.clientX - this.offsetX;
+    this.startY = event.clientY - this.offsetY;
+    this.isDragging = true;
+  }
+
+  private onDrag = (event: DragEvent) => {
+    if (!this.isDragging) {
+      return;
+    }
+    if (event.clientX == 0 && event.clientY == 0) {
+      return;
+    }
+
+    this.offsetX = event.clientX - this.startX;
+    this.offsetY = event.clientY - this.startY;
+    this.updateImagePosition();
+  }
+
+  private onDragEnd = () => {
+    this.isDragging = false;
+  }
+
+  private updateImagePosition() {
+    // These variables immensely helped while writing the clamping logic,
+    const container =  this.image.nativeElement.parentNode;
+    const dialogWidth = container.offsetWidth;
+    const dialogHeight = container.offsetHeight;
+    const imageWidth = this.image.nativeElement.offsetWidth;
+    const imageHeight = this.image.nativeElement.offsetHeight;
+
+    // Clamp how much the image can be dragged towards the right
+    this.offsetX = Math.min(this.offsetX, (imageWidth * this.scale - dialogWidth) / 2);
+    // Clamp how much the image cna be dragged towards the bottom
+    this.offsetY = Math.min(this.offsetY, (imageHeight * this.scale - dialogHeight) / 2);
+    // Clamp how much the image can be dragged towards the left
+    this.offsetX = Math.max(this.offsetX,  -1 * (imageWidth * this.scale - dialogWidth) / 2);
+    // Clamp how much the image can be dragged towards the top
+    this.offsetY = Math.max(this.offsetY,  -1 * (imageHeight * this.scale - dialogHeight) / 2);
+    
+    this.renderer.setStyle(
+      this.image.nativeElement,
+      'transform',
+      `translate(${this.offsetX}px, ${this.offsetY}px) scale(${this.scale})`);
+
+      this.updateCursorStyle();
+  }
+
+  private updateCursorStyle() {
+    this.renderer.setStyle(
+        this.image.nativeElement,
+        'cursor',
+        this.scale <= 1 ? 'zoom-in' : 'grab');
+  }
 
   closeDialog() {
+    if (this.isDragging) {
+      return;
+    }
+
     this.dialogRef.close();
+  }
+
+  ngOnDestroy() {
+    // Cleans up and prevents lingering listeners.
+    document.removeEventListener('dragstart', this.onDragStart);
+    document.removeEventListener('drag', this.onDrag);
+    document.removeEventListener('dragend', this.onDragEnd);
+    document.removeEventListener('wheel', this.onWheelScroll);
   }
 }
